2282b474f06929e8ff8b52210c1b323fe3df2fe4
rameshpeddiy2c31 <100938896+rameshpeddiy2c31@users.noreply.github.com>
2022-03-07 14:53:10 +0530

    Add files via upload

---
+++ TeamsTemplate1/Service/ConversationBot/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+namespace Microsoft.BotBuilderSamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory() + "\\ConversationBot";
+            CreateHostBuilder(args).UseContentRoot(directory).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureLogging((logging) =>
+                    {
+                        logging.AddDebug();
+                        logging.AddConsole();
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
